6b194a28f544bb54dc3438014688afb3cb7b4500
shalominattii-us <shalominattii.us@gmail.com>
2026-07-25 13:47:57 -0600

    Export qualification contracts and engine

--- gov_ops/__init__.py
+++ gov_ops/__init__.py
@@ -1,13 +1,27 @@
-"""Government opportunity ingestion spine."""
+"""Government opportunity ingestion and qualification spine."""
 
 from .infinite_brain import InfiniteBrainPublisher, PublishedIntakeRecord
 from .opportunity import Opportunity
+from .qualification import (
+    DecisionRecord,
+    EvidenceBuilder,
+    EvidencePackage,
+    MissionCandidate,
+    QualificationEngine,
+    QualificationResult,
+)
 from .sam_gov import SAMGovOpportunitiesClient, normalize_sam_opportunity
 
 __all__ = [
+    "DecisionRecord",
+    "EvidenceBuilder",
+    "EvidencePackage",
     "InfiniteBrainPublisher",
+    "MissionCandidate",
     "Opportunity",
     "PublishedIntakeRecord",
+    "QualificationEngine",
+    "QualificationResult",
     "SAMGovOpportunitiesClient",
     "normalize_sam_opportunity",
 ]
