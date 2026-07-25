15675f4d4d81f08ff3b02836f22bdfb402d7b571
shalominattii-us <shalominattii.us@gmail.com>
2026-07-24 18:36:58 -0600

    Export Infinite Brain publisher

--- gov_ops/__init__.py
+++ gov_ops/__init__.py
@@ -1,6 +1,13 @@
 """Government opportunity ingestion spine."""
 
+from .infinite_brain import InfiniteBrainPublisher, PublishedIntakeRecord
 from .opportunity import Opportunity
 from .sam_gov import SAMGovOpportunitiesClient, normalize_sam_opportunity
 
-__all__ = ["Opportunity", "SAMGovOpportunitiesClient", "normalize_sam_opportunity"]
+__all__ = [
+    "InfiniteBrainPublisher",
+    "Opportunity",
+    "PublishedIntakeRecord",
+    "SAMGovOpportunitiesClient",
+    "normalize_sam_opportunity",
+]
